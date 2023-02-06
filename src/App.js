ad20fa279b0ddf109e794f539d8b72bd446117ff
sohaibasaad <46029942+sohaibasaad@users.noreply.github.com>
2023-02-06 14:25:38 +0500

    scroll to top on navlink click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Header from "./components/Header";
 import Footer from './components/Footer';
@@ -18,26 +18,30 @@ import Blogs from './pages/Blogs';
 import BlogDetails from './pages/BlogDetails';
 import TermsAndConditions from './pages/TermsAndConditions';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
 
   return (
     <>
       <Router>
+
         <Header />
-        <Routes>
-          <Route path="*" element={<Navigate to="/home" replace />} />
-          <Route exact path="/home" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/community" element={<Community />} />
-          <Route exact path="/community-fee" element={<CommunityFee />} />
-          <Route exact path="/testimonials" element={<Testimonials />} />
-          <Route exact path="/blogs" element={<Blogs />} />
-          <Route exact path="/blog-details" element={<BlogDetails />} />
-          <Route exact path="/terms-and-conditions" element={<TermsAndConditions />} />
-          <Route exact path="/privacy-policy" element={<PrivacyPolicy />} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            <Route path="*" element={<Navigate to="/home" replace />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/community" element={<Community />} />
+            <Route exact path="/community-fee" element={<CommunityFee />} />
+            <Route exact path="/testimonials" element={<Testimonials />} />
+            <Route exact path="/blogs" element={<Blogs />} />
+            <Route exact path="/blog-details" element={<BlogDetails />} />
+            <Route exact path="/terms-and-conditions" element={<TermsAndConditions />} />
+            <Route exact path="/privacy-policy" element={<PrivacyPolicy />} />
+          </Routes>
+        </ScrollToTop>
         <Footer />
       </Router>
     </>
