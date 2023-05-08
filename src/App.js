a11da6951b48b4e6c12d5e4f8b14a99d4847c0e4
sohaibasaad <46029942+sohaibasaad@users.noreply.github.com>
2023-05-08 10:24:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,17 +29,17 @@ function App() {
         <Header />
         <ScrollToTop>
           <Routes>
-            <Route path="*" element={<Navigate to="/home" replace />} />
-            <Route exact path="/home" element={<Home />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/contact" element={<Contact />} />
-            <Route exact path="/community" element={<Community />} />
-            <Route exact path="/community-fee" element={<CommunityFee />} />
-            <Route exact path="/testimonials" element={<Testimonials />} />
-            <Route exact path="/blogs" element={<Blogs />} />
-            <Route exact path="/blog-details" element={<BlogDetails />} />
-            <Route exact path="/terms-and-conditions" element={<TermsAndConditions />} />
-            <Route exact path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="*" element={<Navigate to="tzort-react/home" replace />} />
+            <Route exact path="tzort-react/home" element={<Home />} />
+            <Route exact path="tzort-react/about" element={<About />} />
+            <Route exact path="tzort-react/contact" element={<Contact />} />
+            <Route exact path="tzort-react/community" element={<Community />} />
+            <Route exact path="tzort-react/community-fee" element={<CommunityFee />} />
+            <Route exact path="tzort-react/testimonials" element={<Testimonials />} />
+            <Route exact path="tzort-react/blogs" element={<Blogs />} />
+            <Route exact path="tzort-react/blog-details" element={<BlogDetails />} />
+            <Route exact path="tzort-react/terms-and-conditions" element={<TermsAndConditions />} />
+            <Route exact path="tzort-react/privacy-policy" element={<PrivacyPolicy />} />
           </Routes>
         </ScrollToTop>
         <Footer />
